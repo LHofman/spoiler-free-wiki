ed2edebce5773e6b90c3a5b000c8225c4bc2c075
Lennert Hofman <hofmanlennert@gmail.com>
2025-05-18 14:12:06 +0200

    Improve PageDetails Screen

--- client/src/components/Page/PageDetails.tsx
+++ client/src/components/Page/PageDetails.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { useState, useEffect, Fragment } from 'react'
+import { useState, useEffect } from 'react'
 import { useAppSelector } from '../../store/hooks';
 import { Link } from '@tanstack/react-router';
 import { PageDetails } from '../../types/PageTypes';
+import { IconEdit } from '@tabler/icons-react';
+import { Button, Card, Collapse, Flex, Group, Text } from '@mantine/core';
 
 interface PageDetailsProps {
   pageId: string;
@@ -11,6 +13,7 @@ interface PageDetailsProps {
 function PageDetailsComponent(props: PageDetailsProps) {
   const progress = useAppSelector((state) => state.progress);
   const [page, setPage] = useState<PageDetails|null>(null);
+  const [openSections, setOpenSections] = useState<number[]>([]);
 
   useEffect(() => {
     const fetchPage = async () => {
@@ -27,26 +30,59 @@ function PageDetailsComponent(props: PageDetailsProps) {
     fetchPage();
   }, [progress, props.pageId]);
 
+  const toggleOpenSection = (index: number) => {
+    if (openSections.includes(index)) {
+      setOpenSections(openSections.filter((i) => i !== index));
+    } else {
+      setOpenSections([...openSections, index]);
+    }
+  }
+
   return page && (
     <>
-      <h1>{ page.title }</h1>
-      <Link to={`/pages/$pageId/edit`} params={{ pageId: page._id }}>Edit</Link>
-      <h2>Properties</h2>
-      { (page.properties ?? []).map((property) => (
-        <p><b>{ property.property }</b>: { property.value }</p>
-      )) }
-      <hr />
-      <h2>Information</h2>
-      { page.text.map((textLine, idx) => <p key={idx}>{ textLine }</p>) }
-      <hr />
-      {
-        page.textSections.map((section, idx) => (
-          <Fragment key={idx}>
-            <h3>{ section.title }</h3>
-            { section.text.map((textLine, idx) => <p key={idx}>{ textLine }</p>) }
-          </Fragment>
-        ))
-      }
+      <h1>
+        { page.title }
+        &nbsp;
+        <Link to={`/pages/$pageId/edit`} params={{ pageId: page._id }}>
+          <IconEdit color='orange' />
+        </Link>
+      </h1>
+      <Flex justify='center' wrap='wrap'>
+        <Card radius='sm' withBorder style={{ margin: '1em', width: '25%', minWidth: '250px' }}>
+          <Card.Section withBorder inheritPadding py="xs">
+            <Group justify="center">
+              <Text fw={500}>Properties</Text>
+            </Group>
+          </Card.Section>
+          <Card.Section style={{ padding: '2em' }}>
+            { (page.properties ?? []).map((property) => (
+              <p><b>{ property.property }</b>: { property.value }</p>
+            )) }
+          </Card.Section>
+        </Card>
+        <div style={{ width: '70%', minWidth: '500px' }}>
+          <Card radius='sm' withBorder style={{ margin: '1em' }}>
+            <Card.Section withBorder inheritPadding py="xs">
+              <Group justify="center">
+                <Text fw={500}>Information</Text>
+              </Group>
+            </Card.Section>
+            <Card.Section style={{ padding: '2em' }}>
+              { page.text.map((textLine, idx) => <p key={idx}>{ textLine }</p>) }
+            </Card.Section>
+          </Card>
+          <Card radius='sm' withBorder style={{ margin: '1em' }}>
+            { page.textSections.map((section, idx) => (
+              <Card.Section withBorder key={idx} style={{ padding: '2em' }}>
+                <h3><Button onClick={() => toggleOpenSection(idx)}>{ section.title }</Button></h3>
+                <Collapse in={openSections.includes(idx)}>
+                  { section.text.map((textLine, idx) => <p key={idx}>{ textLine }</p>) }
+                </Collapse>
+              </Card.Section>
+            )) }
+          </Card>
+        </div>
+      </Flex>
     </>
   );
 }
